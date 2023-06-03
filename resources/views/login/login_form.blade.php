0a6c0bb7cdcb0097eabe533c180667520f4602ce
gytrhu6u-g-2 <gytrhu6u@icloud.com>
2023-06-03 16:35:58 +0900

    ログインフォーム，バリデーションの追加

---
+++ resources/views/login/login_form.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ログインフォーム</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/signin.css') }}">
+</head>
+<body>
+<main class="form-signin w-100 m-auto">
+  <form class="form-signin" method="POST" action="{{ route('login') }}">
+        @csrf
+        <h1 class="h3 mb-3 fw-normal">ログインフォーム</h1>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+        </div>
+        <button class="btn btn-primary w-100 py-2" type="submit">ログイン</button>
+  </form>
+</main>
+</body>
+</html>
