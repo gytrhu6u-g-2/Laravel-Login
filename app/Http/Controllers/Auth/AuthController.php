a39d8921fd7b1d636fdd1ab419daee75b61aed24
gytrhu6u-g-2 <gytrhu6u@icloud.com>
2023-06-10 10:27:13 +0900

    ロック機能の追加

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -32,17 +33,54 @@ class AuthController extends Controller
             ],
         );
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+        // Userのemailに$credentialsのemailを引き出す。存在しない場合はnull
+        $user = User::where('email', '=', $credentials['email'])->first();
 
-            return redirect(route('home'))->with('login_success','ログイン成功しました！');
+        if (!is_null($user)) {
+            // locked_flgが1か判断する
+            if ($user->locked_flg === 1) {
+                return back()->withErrors(
+                    [
+                        'login_error' => 'アカウントがロックされています。'
+                    ]
+                    );
+            }
+
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                // userのerror_countが0より大きい場合
+                if ($user->error_count > 0) {
+                    $user->error_count = 0;
+                    $user->save();
+                }
+    
+                return redirect(route('home'))->with('login_success','ログイン成功しました！');
+            }
         }
+
+        // パスワードが間違っていた場合、error_countを+1する
+        $user->error_count = $user->error_count + 1;
+        // error_countが5より大きい場合
+        if ($user->error_count > 5) {
+            $user->locked_flg = 1;
+            $user->save();
+            return back()->withErrors(
+                [
+                    'login_error' => 'アカウントがロックされました。解除したい場合は運営に問い合わせてください。'
+                ]
+                );
+        }
+        $user->save();
 
         return back()->withErrors(
             [
-                'login_error' => 'メールアドレスかパスワードが間違っています、'
+                'login_error' => 'メールアドレスかパスワードが間違っています。'
             ]
             );
+
+
+
+        
     }
 
     /**
